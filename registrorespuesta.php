e38e41d4667f03e655100dbeaed84199047ba7c7
ESTADIA <19307015@utfv.edu.mx>
2023-03-07 17:13:56 -0600

    7/03/2023

--- registrorespuesta.php
+++ registrorespuesta.php
@@ -3,35 +3,40 @@ error_reporting(E_ALL ^ E_DEPRECATED);
 header("content-Type: text/html; charset=UTF-8");
 
 $correo = isset($_POST['correo']) ? $_POST['correo'] : '';
-$nombre = isset($_POST['nombre']) ? $_POST['nombre'] : '';
-$a_paterno = isset($_POST['a_paterno']) ? $_POST['a_paterno'] : '';
-$a_materno = isset($_POST['a_materno']) ? $_POST['a_materno'] : '';
+$nombre  = isset($_POST['nombre']) ? $_POST['nombre'] : '';
+$pat = isset($_POST['pat']) ? $_POST['pat'] :'';
+$mat = isset($_POST['mat']) ? $_POST['mat'] : '';
 $password = isset($_POST['password'] ) ? $_POST['password'] : '';
+
+$correodos= '';
 
 $correoTabla='';
 $nombreTabla='';
-$a_paternoTabla='';
-$a_maternoTabla='';
+$patTabla='';
+$matTabla='';
 $passwordTabla='';
 
 
 $con = new SQLite3("data.db");
 $cs = $con -> query ("SELECT * FROM login WHERE correo = '$correo'");
-while ($res = $cs -> fetchArray());
-{ $correodos = $res ['correo'];
-    
-    }
-    if ($correodos == $correo) {
-        echo '<script> alert ("ya registrado") </script>';
+
+while ($res = $cs -> fetchArray()) {
+    $correodos = $res ['correo'];
 }
-else
-{
-    $cs = $con -> query ("INSERT INTO login (correo,nombre,a_paterno,a_materno,password) VALUES ('$correo','$nombre','$a_paterno','$a_materno','$password')");
-    echo '<script> alert ("registrado") </script>';
-    echo '<script> window.location = ("login.html") </script>';
+
+if ($correodos == $correo) {
+    echo '<script>alert("Error Usuario Registrado")</script>';
+    echo '<script>window.location="login.html"</script>';
+}else{
+    $csDos = $con -> query ("INSERT INTO login (correo,nombre,pat,mat,password) VALUES ('$correo','$nombre','$pat','$mat','$password')");
+    echo '<script>alert("Registro Correcto")</script>';
+    echo '<script>window.location="login.html"</script>';
 }
 
 
+
+
+?>
 
 // $cs = $con -> query ("INSERT INTO login (correo,nombre,a_paterno,a_materno,password) VALUES ('$correo','$nombre','$a_paterno','$a_materno','$password')");
 
@@ -59,4 +64,3 @@ else
 
    
 
-?>
