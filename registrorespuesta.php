a9e9d69a0ec05c0ad51e098ecedadab14842c3cf
ESTADIA <19307015@utfv.edu.mx>
2023-03-06 18:08:24 -0600

    6/03/2023

--- registrorespuesta.php
+++ registrorespuesta.php
@@ -15,17 +15,42 @@ $a_maternoTabla='';
 $passwordTabla='';
 
 
-
 $con = new SQLite3("data.db");
 $cs = $con -> query ("SELECT * FROM login WHERE correo = '$correo'");
-if ($con -> num_columns > 0){
-    $mensaje.="<h5 class='text-danger text-center'> El correo ya se encuentra registrado</h5>";
-} 
-else{
+while ($res = $cs -> fetchArray());
+{ $correodos = $res ['correo'];
+    
+    }
+    if ($correodos == $correo) {
+        echo '<script> alert ("ya registrado") </script>';
+}
+else
+{
+    $cs = $con -> query ("INSERT INTO login (correo,nombre,a_paterno,a_materno,password) VALUES ('$correo','$nombre','$a_paterno','$a_materno','$password')");
+    echo '<script> alert ("registrado") </script>';
+    echo '<script> window.location = ("login.html") </script>';
+}
 
-$con = new SQLite3("data.db");
-$cs = $con -> query ("INSERT INTO login (correo,nombre,a_paterno,a_materno,password) VALUES ('$correo','$nombre','$a_paterno','$a_materno','$password')");
 
+
+// $cs = $con -> query ("INSERT INTO login (correo,nombre,a_paterno,a_materno,password) VALUES ('$correo','$nombre','$a_paterno','$a_materno','$password')");
+
+// $sql = sprintf("SELECT * FROM login WHERE correo = '$correo'");
+// $resultado = $conn->query($sql);
+// if($resultado){
+//     $_SESSION['correo'] = $correo;
+//     header("login.html"); 
+//   }else{
+//     echo 'El email o password es incorrecto, <a href="index.html">vuelva a intenarlo</a>.<br/>';
+//   }
+
+// $con = new SQLite3("data.db");
+// $cs = $con -> query ("SELECT * FROM login WHERE correo = '$correo'");
+// if ($con -> num_columns > 0){
+//     $mensaje.="<h5 class='text-danger text-center'> El correo ya se encuentra registrado</h5>";
+
+// } 
+// else{
 //echo '
 //<html>
 //<meta http-equiv="REFRESH" content= "1,url=login.html">
@@ -33,5 +58,5 @@ $cs = $con -> query ("INSERT INTO login (correo,nombre,a_paterno,a_materno,passw
 //';
 
    
-}
+
 ?>
